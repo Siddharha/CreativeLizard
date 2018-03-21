b1108ab178c40a6eafcea602dd05f0d331f03bf2
siddhartha <siddhartha.maji@bluehorse.in>
2018-03-21 19:38:34 +0530

    dfsdfsadf

--- creativelizard-base/src/main/java/in/creativelizard/creativelizard/activities/DetailsActivity.java
+++ creativelizard-base/src/main/java/in/creativelizard/creativelizard/activities/DetailsActivity.java
@@ -1,5 +1,7 @@
 package in.creativelizard.creativelizard.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,5 +15,9 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
     }
 }
